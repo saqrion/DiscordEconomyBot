e30ac65315db6eb8730346f09df210d430e427a3
saqrion <xasan0110@gmail.com>
2025-08-29 15:10:29 +0300

    added house model and changed user model

---
+++ DsBot/Models/House.cs
@@ -0,0 +1,9 @@
+﻿namespace DsBot.Models
+{
+    public class House
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Score { get; set; }
+    }
+}
